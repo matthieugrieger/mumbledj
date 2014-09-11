e8b1f2acaf4f9a74bc23e6a171da77f62779677d
Matthieu Grieger <matthieugrieger@gmail.com>
2014-09-11 16:38:12 -0700

    Add run_bot.py

---
+++ run_bot.py
@@ -0,0 +1,13 @@
+#----------------------#
+#   Mumble Music Bot   #
+#  By Matthieu Grieger #
+#----------------------#-------------------------------#
+# run_bot.py                                           #
+# Starts and runs the bot. This is the file you should #
+# execute with Python to start the bot.                #
+#------------------------------------------------------#
+
+#import MusicBot object here
+
+bot = MusicBot()
+bot.connect_to_server()

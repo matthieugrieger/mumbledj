42b79cf14729b866eca09c4eaf273da9b44af468
Matthieu Grieger <matthieugrieger@gmail.com>
2014-09-15 09:46:27 -0700

    Added admin config options

--- mumbledj/config.lua
+++ mumbledj/config.lua
@@ -24,4 +24,57 @@ config.DEFAULT_CHANNEL = "Bot Testing"
 -- DEFAULT VALUE: "!"
 config.COMMAND_PREFIX = "!"
 
+
+-----------------------
+-- ADMIN CONFIGURATION
+-----------------------
+
+-- Enable admins (true = on, false = off)
+-- DEFAULT VALUE: true
+config.ENABLE_ADMINS = true
+
+-- List of admins
+-- NOTE: I recommend only giving users admin privileges if they are registered
+-- on the server. Otherwise people can just take their username and issue admin
+-- commands.
+-- EXAMPLE:
+-- 	config.ADMINS = {"Matt", "Matthieu"}
+config.ADMINS = {}
+
+-- Make start an admin command?
+-- DEFAULT VALUE: false
+config.ADMIN_START = false
+
+-- Make play an admin command?
+-- DEFAULT VALUE: false
+config.ADMIN_PLAY = false
+
+-- Make pause an admin command?
+-- DEFAULT VALUE: false
+config.ADMIN_PAUSE = false
+
+-- Make add an admin command?
+-- DEFAULT VALUE: false
+config.ADMIN_ADD = false
+
+-- Make skip an admin command?
+-- DEFAULT VALUE: false
+config.ADMIN_SKIP = false
+
+-- Make volumeup an admin command?
+-- DEFAULT VALUE: true
+config.ADMIN_VOLUMEUP = true
+
+-- Make volumedown an admin command?
+-- DEFAULT VALUE: true
+config.ADMIN_VOLUMEDOWN = true
+
+-- Make move an admin command?
+-- DEFAULT VALUE: true
+config.ADMIN_MOVE = true
+
+-- Make kill an admin command?
+-- DEFAULT VALUE: true (I recommend never changing this to false)
+config.ADMIN_KILL = true
+
 return config

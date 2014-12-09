409c1374a934fe5f64c875fffc5cf978c08cde40
Matthieu Grieger <matthieugrieger@gmail.com>
2014-12-08 21:45:22 -0800

    Add main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+/*
+ * MumbleDJ
+ * By Matthieu Grieger
+ * main.go
+ * Copyright (c) 2014 Matthieu Grieger (MIT License)
+ */
+
+package main
+
+import (
+	"github.com/layeh/gumble/gumble"
+	"github.com/layeh/gumble/gumble_ffmpeg"
+	"github.com/layeh/gumble/gumbleutil"
+)
+
+// MumbleDJ type declaration
+type MumbleDJ struct {
+	serverAddress	string
+	serverPort		int
+	username		string
+	password		string
+}

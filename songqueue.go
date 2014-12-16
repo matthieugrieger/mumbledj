6924df7f13a68b55881334211896245b3220327f
Matthieu Grieger <matthieugrieger@gmail.com>
2014-12-15 16:41:58 -0800

    Add some unfinished methods to SongQueue

--- songqueue.go
+++ songqueue.go
@@ -5,12 +5,24 @@
  * Copyright (c) 2014 Matthieu Grieger (MIT License)
  */
 
- package main
+package main
 
- type SongQueue struct {
- 	songs []Song
- }
+type SongQueue struct {
+	queue Queue
+}
 
- func NewSongQueue() *SongQueue {
- 	return &SongQueue{}
- }
+func NewSongQueue() *SongQueue {
+	return &SongQueue{}
+}
+
+func (q *SongQueue) AddSong(s *Song) bool {
+	return false
+}
+
+func (q *SongQueue) NextSong() bool {
+	return false
+}
+
+//func (q *SongQueue) CurrentSong() Song {
+//	return false
+//}

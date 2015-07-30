a37dbe128325b7e6b0faf64046963d98caeb6eb2
MichaelOultram <MichaelOultram@users.noreply.github.com>
2015-07-30 14:28:39 +0100

    Fixing build issues

--- web.go
+++ web.go
@@ -83,7 +83,7 @@ func (web *WebServer) skip(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (website *websiteServer) GetwebsiteAddress(user *gumble.User) {
+func (website *WebServer) GetWebAddress(user *gumble.User) {
 	if website.client_token[user] != "" {
 		website.token_client[website.client_token[user]] = nil
 	}

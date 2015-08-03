334c1fcfe4fc8242e2706c4205bbe7e5bf055095
MichaelOultram <MichaelOultram@users.noreply.github.com>
2015-08-03 22:33:39 +0100

    Updated .travis.yml

---
+++ install-dependencies.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# check to see if ffmpeg is installed
+if [ ! -f "$HOME/bin/ffmpeg" ]; then
+    wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -O /tmp/ffmpeg.tar.xz
+    tar -xvf /tmp/ffmpeg.tar.xz --strip 1 --no-anchored ffmpeg ffprobe
+    chmod a+rx ffmpeg ffprobe
+    mv ff* $HOME/bin
+else
+  echo 'Using cached version of ffmpeg.';
+fi
+
+# check to see if opus is installed
+if [ ! -d "$HOME/opus/lib" ]; then
+    wget http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
+    tar xzvf opus-1.0.3.tar.gz
+    cd opus-1.0.3 && ./configure --prefix=$HOME/opus && make && make install
+else
+  echo 'Using cached version of opus.';
+fi
+
+# check to see if youtube-dl is installed
+if [ ! -f "$HOME/bin/youtube-dl" ]; then
+    curl https://yt-dl.org/downloads/2015.07.28/youtube-dl -o ~/bin/youtube-dl
+    chmod a+rx ~/bin/youtube-dl
+else
+  echo 'Using cached version of youtube-dl.';
+fi

4520bac5140fe4684c5d4cf691e5e6e328724232
MichaelOultram <MichaelOultram@users.noreply.github.com>
2015-08-13 14:57:12 +0100

    Soundcloud API works, just no music [ci skip]

--- youtube_dl.go
+++ youtube_dl.go
@@ -74,7 +74,7 @@ func (dl *YouTubeDLSong) Play() {
 		panic(err)
 	} else {
 		message := `<table><tr><td align="center"><img src="%s" width=150 /></td></tr><tr><td align="center"><b><a href="%s">%s</a> (%s)</b></td></tr><tr><td align="center">Added by %s</td></tr>`
-		message = fmt.Sprintf(message, dl.thumbnail, dl.url, dl.title, dl.duration, dl.submitter)
+		message = fmt.Sprintf(message, dl.thumbnail, dl.url, dl.title, dl.duration, dl.submitter.Name)
 		if !isNil(dl.playlist) {
 			message = fmt.Sprintf(message+`<tr><td align="center">From playlist "%s"</td></tr>`, dl.playlist.Title())
 		}

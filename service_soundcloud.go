92c8230ae80cad17ee4814f188d952ff251d19e9
MichaelOultram <MichaelOultram@users.noreply.github.com>
2015-08-16 19:10:49 +0100

    Fixing build issues

--- service_soundcloud.go
+++ service_soundcloud.go
@@ -64,8 +64,8 @@ func (sc SoundCloud) NewRequest(user *gumble.User, url string) (string, error) {
 			timesplit = strings.Split(timesplit[1], ":")
 			multiplier := 1
 			for i := len(timesplit) - 1; i >= 0; i-- {
-				offset += strconv.Atoa(timesplit[i]) * multiplier
-				mutiplier *= 60
+				offset += strconv.Atoi(timesplit[i]) * multiplier
+				multiplier *= 60
 			}
 			fmt.Printf("Offset: " + strconv.Itoa(offset)) // DEBUG
 		}
